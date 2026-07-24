e272207af45b9eca03221e09a317a0df03dad7fa
Alena <alena.kozlova2004@mail.ru>
2026-07-24 18:02:21 +0300

    add settings

---
+++ config.py
@@ -0,0 +1,19 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore")
+
+    opensearch_host: str = "localhost"
+    opensearch_port: int = 9200
+    opensearch_use_ssl: bool = False
+
+    index_name: str = Field(default="knowledge_base", alias="OPENSEARCH_INDEX")
+
+    embedding_model: str = "intfloat/multilingual-e5-small"
+    embedding_dim: int = 384
+
+
+settings = Settings()
